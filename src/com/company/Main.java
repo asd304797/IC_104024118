5f14636aa2620ca55d35ce0fd57d3003635b8566
asd304797 <asd304797@gmail.com>
2017-09-25 15:47:28 +0800

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-	System.out.println("104024118王普鑫");
-        System.out.println("資傳系");
+	System.out.println("104024118\n王普鑫\n資傳系");
+        System.out.println("\\\n\"");
     }
 }
